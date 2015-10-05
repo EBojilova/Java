98637e619e538bdc6e8a55f2dce90af909eeb395
Elena Bojilova <elena_bogoeva2003@yahoo.co.uk>
2015-10-05 13:11:06 +0300

    Ammendments to DraconAccounting

--- ExamPreparation/src/DragonAccountingExam.java
+++ ExamPreparation/src/DragonAccountingExam.java
@@ -6,62 +6,66 @@ import java.util.Scanner;
 
 public class DragonAccountingExam {
 
-    public static BigDecimal incerase = new BigDecimal(1.006);
+    public static BigDecimal Increase = new BigDecimal(1.006);
+
+    public static BigDecimal Month = new BigDecimal(30);
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         BigDecimal capital = new BigDecimal(scanner.nextLine());
         List<BigDecimal> salaries = new ArrayList<>();
-        List<Integer> hiredWorkers = new ArrayList<>();
+        List<Long> hiredWorkersDay = new ArrayList<>();
         String input;
-        int days = 0;
+        int days = 1;
         while (!(input = scanner.nextLine()).equals("END")) {
 //            {hired};{fired};{salary};{additional events: money},
-//            10;0;40;Machines:120.4
-//            7;6;44;Product development:8.8;Taxes:12
             String[] inputData = input.split(";");
-            int hired = Integer.parseInt(inputData[0]);
-            int fired = Integer.parseInt(inputData[1]);
-            BigDecimal salary = new BigDecimal(30);
-            BigDecimal dailySlary = new BigDecimal(inputData[2]).divide(salary, 9, RoundingMode.FLOOR).setScale(7, BigDecimal.ROUND_DOWN);
+            long hired = Long.parseLong(inputData[0]);
+            long fired = Long.parseLong(inputData[1]);
+            BigDecimal salary = new BigDecimal(inputData[2]);
 
-            // hire employees ->
-            salaries.add(dailySlary);
-            hiredWorkers.add(hired);
+            // hire employees -> working days= days-index;
+            salaries.add(salary);
+            hiredWorkersDay.add(hired);
 
             // check for raise ->
-            for (int day = 365; day < salaries.size(); day++) {
-                if (day % 365 == 0 && !salaries.get(day)
-                                               .equals(BigDecimal.ZERO)) {
-                    BigDecimal incerase = new BigDecimal(1.006);
-                    BigDecimal increasedDailySalary = salaries.get(day)
-                                                              .multiply(incerase);
-                    salaries.set(day, increasedDailySalary);
+            for (int day = 364; day < salaries.size(); day++) {
+                int workingDays = days - day;
+                if (workingDays % 365 == 0 && !hiredWorkersDay.get(day)
+                                                              .equals(BigDecimal.ZERO)) {
+                    BigDecimal increasedSalary = salaries.get(day)
+                                                         .multiply(Increase);
+                    salaries.set(day, increasedSalary);
                 }
             }
             // give salaries ->
             if (days % 30 == 0) {
-                for (int i = 0; i < days; i++) {
-                    int workingDaysMonth = (i) % 30 + 1;
+                for (int day = 0; day < days; day++) {
+                    if (!hiredWorkersDay.get(day)
+                                        .equals(BigDecimal.ZERO)) {
+                        int workingDays = days - day;
+                        int workingDaysMonth = workingDays % 30;
 
-                    BigDecimal paiedSalaries = salaries.get(i)
-                                                       .multiply(new BigDecimal(workingDaysMonth))
-                                                       .multiply(new BigDecimal(hiredWorkers.get(i)));
-                    capital = capital.subtract(paiedSalaries);
+                        BigDecimal paiedSalaries = salaries.get(day)
+                                                           .divide(Month, 9, RoundingMode.FLOOR)
+                                                           .setScale(7, BigDecimal.ROUND_DOWN)
+                                                           .multiply(new BigDecimal(workingDaysMonth))
+                                                           .multiply(new BigDecimal(hiredWorkersDay.get(day)));
+                        capital = capital.subtract(paiedSalaries);
+                    }
                 }
             }
             // fire employees ->
             int day = 0;
             while (fired > 0 && day < days) {
-                int firedDay = Math.min(fired, hiredWorkers.get(day));
-//                BigDecimal paiedSalaries = salaries.get(day)
-//                                                   .multiply(new BigDecimal(days % 30))
-//                                                   .multiply(new BigDecimal(fired));
-//                capital = capital.subtract(paiedSalaries);
-
-                int leftWorkersDay = hiredWorkers.get(day) - firedDay;
-                hiredWorkers.set(day, leftWorkersDay);
-                day++;
+                if (!hiredWorkersDay.get(day)
+                                    .equals(BigDecimal.ZERO)) {
+                    long firedDay = Math.min(fired, hiredWorkersDay.get(day));
+                    long leftWorkersDay = hiredWorkersDay.get(day) - firedDay;
+                    hiredWorkersDay.set(day, leftWorkersDay);
+                    fired -= firedDay;
+                    day++;
+                }
             }
             // check for additional income/expense ->
             for (int i = 3; i < inputData.length; i++) {
@@ -77,16 +81,17 @@ public class DragonAccountingExam {
             }
             // check for bankruptcy.
             if (capital.compareTo(new BigDecimal(0)) <= 0) {
-                System.out.printf("BANKRUPTCY: %.4f", capital.abs().setScale(4, BigDecimal.ROUND_DOWN))
+                System.out.printf("BANKRUPTCY: %.4f", capital.abs()
+                                                             .setScale(4, BigDecimal.ROUND_DOWN))
                           .println();
                 return;
             }
 
             days++;
         }
-        System.out.printf("%d %.4f", hiredWorkers.stream()
-                                                 .mapToInt(x -> x)
-                                                 .sum(), capital.setScale(4, BigDecimal.ROUND_DOWN))
+        System.out.printf("%d %.4f", hiredWorkersDay.stream()
+                                                    .mapToLong(x -> x)
+                                                    .sum(), capital.setScale(4, BigDecimal.ROUND_DOWN))
                   .println();
     }
 }
